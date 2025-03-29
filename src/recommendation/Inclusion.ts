0b07a37ed1c91f48cb9a79d604cdad5d66450ebe
Steffen <60031751+steffenhansendev@users.noreply.github.com>
2025-03-29 19:35:32 +0100

    Update Inclusion.ts with readonly

--- src/recommendation/Inclusion.ts
+++ src/recommendation/Inclusion.ts
@@ -4,23 +4,23 @@ export interface Inclusion {
 
 export interface StallType extends Inclusion {
     // To allow the back end to change these without breaking the front, plain string was chosen over enum and a type union of strings
-    value: string;
+    readonly value: string;
 }
 
 export interface ParkingLot extends Inclusion {
-    id: string;
-    name: string | undefined;
-    capacities: StallCount[];
-    location?: Coordinates;
+    readonly id: string;
+    readonly name: string | undefined;
+    readonly capacities: StallCount[];
+    readonly location?: Coordinates;
     distanceFromUserInMeters?: number
 }
 
 export interface Coordinates {
-    longitude: number;
-    latitude: number;
+    readonly longitude: number;
+    readonly latitude: number;
 }
 
 export interface StallCount {
-    stallType: string,
-    count: number
+    readonly stallType: string;
+    readonly count: number;
 }
