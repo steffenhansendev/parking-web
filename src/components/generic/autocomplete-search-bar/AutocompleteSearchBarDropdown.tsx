4435036202cfe715957f8acd7c2d1185c3d401f9
Steffen Hansen <mail@steffen-hansen.dk>
2025-04-08 11:22:13 +0200

    Extract constants for class names

--- src/components/generic/autocomplete-search-bar/AutocompleteSearchBarDropdown.tsx
+++ src/components/generic/autocomplete-search-bar/AutocompleteSearchBarDropdown.tsx
@@ -1,10 +1,12 @@
 import React, {JSX} from "react";
-
 import {AutocompleteOptionView} from "./AutocompleteOptionView";
 
-const ACTIVE_LI_ELEMENT_CLASS: string = "active";
-const MOUSE_OVER_LI_ELEMENT_CLASS: string = "bg-dark-subtle";
-const UL_ELEMENT_STYLE: React.CSSProperties = {width: "100%", cursor: "default", display: "block"}
+const LI_ELEMENT_CLASS_NAMES: string[] = ["dropdown-item"];
+const ACTIVE_LI_ELEMENT_CLASS_NAMES: string[] = ["active"];
+const MOUSE_OVER_LI_ELEMENT_CLASS_NAMES: string[] = ["bg-dark-subtle"];
+
+const UL_ELEMENT_CLASS_NAMES: string[] = ["dropdown-menu"];
+const UL_ELEMENT_STYLE: React.CSSProperties = {width: "100%", cursor: "default", display: "block"};
 
 interface Props {
     options: AutocompleteOptionView[];
@@ -13,21 +15,30 @@ interface Props {
 }
 
 function AutocompleteSearchBarDropdown({options, activeLiElementIndex, chooseOption}: Props): JSX.Element {
+    const getLiElementClassNames = (i: number): string => {
+        let liElementClassNames: string[] = LI_ELEMENT_CLASS_NAMES;
+        if (activeLiElementIndex === i) {
+            liElementClassNames = liElementClassNames.concat(ACTIVE_LI_ELEMENT_CLASS_NAMES);
+        }
+        return liElementClassNames.join(" ");
+    };
+    const mouseOverLiElementClassNames: string = MOUSE_OVER_LI_ELEMENT_CLASS_NAMES.join(" ");
+
     return (
         <ul
             // Rather than a list of <Option> elements, <li> and <ul> were chosen because:
             // Using <Option> will cause the dropdown to never show again whenever an option is chosen.
             // Using <Option> offers no way of distinguishing whether an option was chosen or the <Input>'s value was changed.
-            className={"dropdown-menu"}
+            className={UL_ELEMENT_CLASS_NAMES.join(" ")}
             style={UL_ELEMENT_STYLE}>
             {options.map((option: AutocompleteOptionView, i: number) => {
                 return <li
-                    className={activeLiElementIndex === i ? `dropdown-item ${ACTIVE_LI_ELEMENT_CLASS}` : "dropdown-item"}
+                    className={getLiElementClassNames(i)}
                     onMouseOver={(e): void => {
-                        e.currentTarget.classList.add(MOUSE_OVER_LI_ELEMENT_CLASS);
+                        e.currentTarget.classList.add(mouseOverLiElementClassNames);
                     }}
                     onMouseOut={(e): void => {
-                        e.currentTarget.classList.remove(MOUSE_OVER_LI_ELEMENT_CLASS);
+                        e.currentTarget.classList.remove(mouseOverLiElementClassNames);
                     }}
                     onMouseDown={async () => {
                         await chooseOption(option);
@@ -40,5 +51,6 @@ function AutocompleteSearchBarDropdown({options, activeLiElementIndex, chooseOpt
         </ul>
     );
 }
+
 
 export default AutocompleteSearchBarDropdown;
